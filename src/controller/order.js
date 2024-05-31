0144312967e7114d33e04d0fba09d445775bd95c
Solveig <132928451+SolveigBeen@users.noreply.github.com>
2024-05-31 09:13:36 +0200

    Updated addressing database

--- src/controller/order.js
+++ src/controller/order.js
@@ -1,20 +1,13 @@
-import nedb from 'nedb-promises';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const dbPath = path.join(__dirname, '../database/orders.db');
-const ordersDB = new nedb({ filename: dbPath, autoload: true });
+import db from '../database/database.js';
 
 /* ordersDB.insert({ customerID: '111', date: "2024-05-30 18:50", products:"kaffe", quantity:4, pricePerUnit:35, _id:5555 
 }); */
 
+//Serach for all orders in orders database made by customer with selected customerId.
 
 async function getOrdersByCustomerId(customerId) {
   try{
-   const orders = await ordersDB.find({ customerID: customerId });
+   const orders = await db.orders.find({ customerID: customerId });
    return orders; 
   }catch (error){
     console.error("Error fetching orders", error);
