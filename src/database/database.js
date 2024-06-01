340f2dfaac577a1d487c2cbe02548fec86c1e137
Andreas Bergström <andreas@textalkgroup.com>
2024-06-01 21:00:41 +0200

    added a few checks in middleware

--- src/database/database.js
+++ src/database/database.js
@@ -7,11 +7,13 @@ const __dirname = path.dirname(__filename);
 
 const database_names = ["company", "cart", "customers", "menu", "orders"];
 const db = {};
+
 database_names.forEach((name) => {
-  const filename = `${__dirname}\\${name}.db`;
-  db[name] = new nedb({
-    filename: filename,
-    autoload: true,
-  });
+const filename = path.join(__dirname, `${name}.db`);
+db[name] = new nedb({
+filename: filename,
+autoload: true,
 });
+});
+
 export default db;
--- src/middleware/checkProductExists.js
+++ src/middleware/checkProductExists.js
@@ -2,24 +2,62 @@ import db from "../database/database.js";
 
 async function checkProductExists(req, res, next) {
   const productID = req.body.product;
+  const customerID = req.body.customerID;
+  const cartID = req.body.cartID;
   console.log(productID);
   try {
-    const coffeeQuery = await db.menu.findOne({ _id: productID });
+    const productQuery = await db.menu.findOne({ _id: productID });
 
-    if (!coffeeQuery) {
+    if (!productQuery) {
       return res
         .status(404)
         .json({ error: "Product not found in the database" });
     }
-
-    // Attach the product to the req object
-    req.product = coffeeQuery;
-
-    // Proceed to the next middleware or route handler
-    next();
   } catch (error) {
     console.error("Error querying the database:", error);
     return res.status(500).json({ error: "Internal server error" });
   }
+  if (cartID && customerID) {
+    try {
+      const cartQuery = await db.cart.findOne({
+        _id: cartID,
+        customerID: customerID,
+      });
+      if (!cartQuery) {
+        return res.status(404).json({
+          error: "Cart with customerID and cartID  not found in database",
+        });
+      }
+    } catch (error) {
+      console.error("Error querying the database:", error);
+      return res.status(500).json({ error: "Internal server error" });
+    }
+  } else {
+    if (cartID) {
+      try {
+        const cartQuery = await db.cart.findOne({ _id: cartID });
+        if (!cartQuery) {
+          return res.status(404).json({ error: "Cart not found in database" });
+        }
+      } catch (error) {
+        console.error("Error querying the database:", error);
+        return res.status(500).json({ error: "Internal server error" });
+      }
+    }
+    if (customerID) {
+      try {
+        const customerQuery = await db.customers.findOne({ _id: customerID });
+        if (!customerQuery) {
+          return res
+            .status(404)
+            .json({ error: "Customer not found in database" });
+        }
+      } catch (error) {
+        console.error("Error querying the database:", error);
+        return res.status(500).json({ error: "Internal server error" });
+      }
+    }
+  }
+  next();
 }
 export default checkProductExists;
