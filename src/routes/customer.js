de14e25d5db177bc128fe041a4ea970f128426f7
Emma Dybdorf <emma.dybdorf@hotmail.com>
2024-06-03 10:06:07 +0200

    Update customer.js

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,18 +1,8 @@
 import express from 'express';
 import { register, login } from '../controller/customer.js';
-import nedb from 'nedb-promises';
-import path from 'path';
+import customersDB from '../database/database.js';
 
 const router = express.Router();
-
-// Specify the path to the database file relative to the root directory of the project
-const dbPath = path.join(process.cwd(), 'src', 'database', 'customers.db');
-
-// Create an instance of the nedb database
-const customersDB = nedb.create({ filename: dbPath, autoload: true });
-
-// Log the path to the database file
-console.log("Database path:", dbPath);
 
 // Endpoint for user registration
 router.post("/register", register);
@@ -24,9 +14,14 @@ router.post("/login", login);
 router.get("/", async (req, res) => {
   try {
       // Fetch all customers from the database
-      const customers = await customersDB.find({});
-      // Send back all customers as a response
-      res.json(customers);
+      const customers = await customersDB.customers.find({});
+      if (customers) {
+          // Send back all customers as a response
+          res.json(customers);
+      } else {
+          // No customers found
+          res.status(404).json({ error: "No customers found" });
+      }
   } catch (error) {
       // Handle errors if customers cannot be fetched
       console.error("Error fetching customers:", error);
