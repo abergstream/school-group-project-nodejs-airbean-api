f3a81e44483f530b09535da22b16218586ead6ee
Andreas Bergström <skrap@andreasb.se>
2024-06-03 14:34:33 +0200

    small fix

--- src/routes/confirmation.js
+++ src/routes/confirmation.js
@@ -1,4 +1,4 @@
-// Confirmation route to view the placed order 
+// Confirmation route to view the placed order
 // To give info when order is delivered, we assume it takes 20 min from when the order was made.
 
 import { Router } from "express";
@@ -9,14 +9,17 @@ const router = Router();
 //Sök order genom att ange order-id:
 router.get("/:id", async (req, res) => {
   try {
-    const orderId = parseInt(req.params.id, 10); // Säkerställ att orderId är ett nummer och 10 anger att strängen ska tolkas som ett decimaltal (bas 10)
+    const orderId = req.params.id; // Säkerställ att orderId är ett nummer och 10 anger att strängen ska tolkas som ett decimaltal (bas 10)
     const order = await getOrderByOrderId(orderId);
     if (order) {
       //Beräkna tid för leverans (20 min)
       const orderDate = new Date(order.date);
-      const deliveryDate = new Date(orderDate.getTime() + 20 * 60000);   //Räkna om till millisekunder
+      const deliveryDate = new Date(orderDate.getTime() + 20 * 60000); //Räkna om till millisekunder
       // Gör formatet till HH:MM
-      const deliveryTime = deliveryDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+      const deliveryTime = deliveryDate.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
 
       // Returnera order med leveranstid
       res.json({ ...order, deliveryTime });
@@ -24,7 +27,9 @@ router.get("/:id", async (req, res) => {
       res.status(404).json({ message: "Order not found" });
     }
   } catch (error) {
-    res.status(500).json({ message: "Error fetching order", error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error fetching order", error: error.message });
   }
 });
 
