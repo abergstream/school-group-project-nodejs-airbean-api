a865ee036778a9c93e8a1b39051f2dd444ff8b8c
Andreas Bergström <andreas@textalkgroup.com>
2024-06-05 10:55:25 +0200

    Fixed default export

--- src/controller/info.js
+++ src/controller/info.js
@@ -1,14 +1,14 @@
 import db from "../database/database.js";
 
 const getCompanyInfo = async () => {
-    try {
-        console.log("Trying to get company info")
-        const companyInfo = await db["company"].find({})
-        console.log("GOT company info")
-        return companyInfo
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    console.log("Trying to get company info");
+    const companyInfo = await db["company"].find({});
+    console.log("GOT company info");
+    return companyInfo;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export { getCompanyInfo }
+export default getCompanyInfo;
